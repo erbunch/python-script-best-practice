b89c30fe6a874e57ca503908052bd6d900161489
Erin R. Bunch <erinbunch2801@live.com>
2023-09-17 18:37:22 -0600

    Best Practices 2: adding a module to allow it to be imported.

--- smallest_factor.py
+++ smallest_factor.py
@@ -9,20 +9,21 @@ def get_smallest_prime_factor(n):
                     if (n % i) == 0:
                                     return i
                                     return None
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        sys.exit(sys.argv[0] + " : Expecting one command line argument the integer for which to get the smallest factor")
+    n = int(sys.argv[1])
+    if n < 1:
+        sys.exit(sys.argv[0] + ": Expecting a positive integer")
 
-if len(sys.argv) != 2:
-    sys.exit(sys.argv[0] + " : Expecting one command line argument the integer for which to get the smallest factor")
-n = int(sys.argv[1])
-if n < 1:
-    sys.exit(sys.argv[0] + ": Expecting a positive integer")
-smallest_prime_factor = None
+    smallest_prime_factor = None
 
-for i in range(2, n):
-    if (n % i) == 0:
-        smallest_prime_factor = i
-        break
+    for i in range(2, n):
+        if (n % i) == 0:
+            smallest_prime_factor = i
+            break
 
-if smallest_prime_factor is None:
-    print(n)
-else:
-    print(smallest_prime_factor)
+    if smallest_prime_factor is None:
+        print(n)
+    else:
+        print(smallest_prime_factor)
